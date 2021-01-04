b49e52595dcd90eef4825b588e03291ff68e7e45
Brandon Estano <estanob@gmail.com>
2021-01-04 01:40:18 -0800

    Complete validations

--- W7D1/rails_auth/app_name/app/models/user.rb
+++ W7D1/rails_auth/app_name/app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :session_token, presence: true
   validates_presence_of :password_digest, message: "Password can't be blank"
+  validates :password, length: {minimum: 6, allow_nil: true}
 
+  before_validation :ensure_session_token
+
+  
 end
