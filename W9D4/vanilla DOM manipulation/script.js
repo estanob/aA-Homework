1c9a81fa6f1643e402f537450e55e93826ea5c20
Brandon <estanob@gmail.com>
2021-01-22 00:11:49 -0800

    Work on second problem

--- W9D4/vanilla DOM manipulation/script.js
+++ W9D4/vanilla DOM manipulation/script.js
@@ -18,6 +18,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // adding SF places as list items
 
+  const sFPlaces = document.getElementById("list-container");
+  sFPlaces.addEventListener("submit", e => {
+    e.preventDefault();
+  })
+
+  const sFPlaceNameInputEl = document.getElementById("sf-places-input")
+  const favoriteInput = sFPlaceNameInputEl.value;
+  sFPlaceNameInputEl.value = '';
+
+  const ul = document.getElementById("sf-places");
+  const li = document.createElement("li");
+  li.textContent = favoriteInput;
+
+  ul.appendChild(li);
   // --- your code here!
 
 
