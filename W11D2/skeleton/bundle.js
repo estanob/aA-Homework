6dac3907cd48f0aa90a0bd53b36266d11618547a
Brandon <estanob@gmail.com>
2021-02-03 01:33:17 -0800

    Set up rainbow color routes

--- W11D2/skeleton/bundle.js
+++ W11D2/skeleton/bundle.js
@@ -275,7 +275,32 @@ function (_React$Component) {
     value: function render() {
       return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Rainbow Router!"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         id: "rainbow"
-      }));
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
+        exact: true,
+        path: "/",
+        component: Rainbow
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
+        path: "/red",
+        component: _red__WEBPACK_IMPORTED_MODULE_2__["default"]
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
+        path: "/red/orange",
+        component: Orange
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
+        path: "/red/yellow",
+        component: Yellow
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
+        path: "/green",
+        component: _green__WEBPACK_IMPORTED_MODULE_3__["default"]
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
+        path: "/blue",
+        component: _blue__WEBPACK_IMPORTED_MODULE_4__["default"]
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
+        path: "/blue/indigo",
+        component: Indigo
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
+        path: "/violet",
+        component: _violet__WEBPACK_IMPORTED_MODULE_5__["default"]
+      })));
     }
   }]);
 
--- W11D2/skeleton/components/rainbow.jsx
+++ W11D2/skeleton/components/rainbow.jsx
@@ -16,9 +16,16 @@ class Rainbow extends React.Component {
       <div>
         <h1>Rainbow Router!</h1>
         {/* Your links should go here */}
-
         <div id="rainbow">
           {/* Your routes should go here */}
+          <Route exact path="/" component={Rainbow} />
+          <Route path="/red" component={Red} />
+          <Route path="/red/orange" component={Orange} />
+          <Route path="/red/yellow" component={Yellow} />
+          <Route path="/green" component={Green} />
+          <Route path="/blue" component={Blue} />
+          <Route path="/blue/indigo" component={Indigo} />
+          <Route path="/violet" component={Violet} />
         </div>
       </div>
     );
